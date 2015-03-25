9119500e7d4fc1836d39c3b027908c88074d585c
NodCaleb <nod.caleb@gmail.com>
2015-03-25 21:12:00 +0200

    Initial Commit

---
+++ Fitness Trainer/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  Fitness Trainer
+//
+//  Created by Eugene Rozhkov on 25.03.15.
+//  Copyright (c) 2015 Nord Point. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
